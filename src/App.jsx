bf966a7d0336c6d660e4dc0b449d95d19b6524e0
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-08-02 10:25:59 +0300

    step 5 - localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from 'nanoid'
 import ContactForm from './components/ContactForm/ContactForm'
 import ContactList from './components/ContactList/ContactList'
 import SearchBox from './components/SearchBox/SearchBox'
 
 const App = () => {
-    const [contacts, setContacts] = useState([
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ])
+    const [contacts, setContacts] = useState(() => {
+        const storedContacts = localStorage.getItem('contacts')
+        return storedContacts
+            ? JSON.parse(storedContacts)
+            : [
+                { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+                { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+                { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+                { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+            ]
+    })
 
     const [filter, setFilter] = useState('')
 
@@ -36,11 +41,15 @@ const App = () => {
         contact.name.toLowerCase().includes(filter.toLowerCase())
     )
 
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts))
+    }, [contacts])
+
     return (
         <div className="container">
+            <h1 className="phonebook-title">Phonebook</h1>
             <ContactForm onAdd={handleAddContact} />
             <SearchBox value={filter} onChange={handleFilterChange} />
-            <h1 className="phonebook-title">Phonebook</h1>
             <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
         </div>
     )
